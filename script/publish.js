3a93e8abe3e684b1d1a424923ea99aeb266e676a
白云苍狗 <admin@imalun.com>
2022-09-08 16:25:09 +0800

    🐞 fix: 路径取值错误

--- script/publish.js
+++ script/publish.js
@@ -1,9 +1,11 @@
 const fs = require('fs')
+const path = require('path')
 
 if (process.env.RELEASE_VERSION) {
-    console.log(process.env.RELEASE_VERSION);
+    const version = process.env.RELEASE_VERSION.replace(/[Vv]/g, '')
+    console.log('当前版本：', version);
     const pkg = require('../package/hexo-theme-async/package.json')
-    pkg.version = process.env.RELEASE_VERSION
-    fs.writeFileSync('../package/hexo-theme-async/package.json', JSON.stringify(pkg, null, 4), 'utf-8')
+    pkg.version = version
+    fs.writeFileSync(path.resolve('../package/hexo-theme-async/package.json'), JSON.stringify(pkg, null, 4), 'utf-8')
 }
 
