f79fbec7cb69fb39f9c561dd24ff85896d88e515
白云苍狗 <admin@imalun.com>
2022-09-08 16:41:54 +0800

    🐞 fix: 路径异常

--- script/publish.js
+++ script/publish.js
@@ -2,10 +2,10 @@ const fs = require('fs')
 const path = require('path')
 
 if (process.env.RELEASE_VERSION) {
-    const version = process.env.RELEASE_VERSION.replace(/[Vv]/g, '')
+    const version = process.env.RELEASE_VERSION.split('/').reverse()[0]
     console.log('当前版本：', version);
     const pkg = require('../package/hexo-theme-async/package.json')
     pkg.version = version
-    fs.writeFileSync(path.resolve('../package/hexo-theme-async/package.json'), JSON.stringify(pkg, null, 4), 'utf-8')
+    fs.writeFileSync(path.resolve(__dirname, '../package/hexo-theme-async/package.json'), JSON.stringify(pkg, null, 4), 'utf-8')
 }
 
