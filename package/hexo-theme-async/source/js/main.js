2e750970aab990c79cef92e913f4d221daeae6c0
白云苍狗 <admin@imalun.com>
2022-09-20 10:38:51 +0800

    🦄 refactor: 抽离本地搜索功能

--- package/hexo-theme-async/source/js/main.js
+++ package/hexo-theme-async/source/js/main.js
@@ -238,108 +238,6 @@
             })
           }
         })
-    },
-    GetLocalSearch: (() => {
-      let localSearch = null;
-      return () => {
-        if (!LocalSearch) {
-          return;
-        }
-        if (!localSearch) {
-          const { search, root } = window.ASYNC_CONFIG;
-          localSearch = new LocalSearch({
-            path: root + search.path,
-            top_n_per_article: search.top_n_per_article,
-            unescape: search.unescape,
-          })
-        }
-        return localSearch
-      }
-    })(),
-    InitSearch() {
-      const { search, i18n } = ASYNC_CONFIG;
-      if (search.enable && search.type === 'local') {
-        if (!search.path) {
-          console.warn('`hexo-generator-searchdb` plugin is not installed!')
-          return;
-        }
-
-        const searchBtn = utils.q('#trm-search-btn')
-        const closeBtn = utils.q('.trm-search-popup-btn-close')
-        const input = utils.q('.trm-search-input')
-        const localSearch = utils.GetLocalSearch()
-
-        if (search.preload && !localSearch.isfetched)
-          localSearch.fetchData()
-
-        const inputEventFunction = () => {
-          if (!localSearch.isfetched)
-            return
-          const searchText = input.value.trim().toLowerCase()
-          const keywords = searchText.split(/[-\s]+/)
-          const container = document.querySelector('.trm-search-result-container')
-          let resultItems = []
-
-          if (searchText.length > 0) {
-            resultItems = localSearch.getResultItems(keywords)
-          }
-
-          if (resultItems.length > 0) {
-            resultItems.sort((left, right) => {
-              if (left.includedCount !== right.includedCount)
-                return right.includedCount - left.includedCount
-
-              else if (left.hitCount !== right.hitCount)
-                return right.hitCount - left.hitCount
-
-              return right.id - left.id
-            })
-            const stats = i18n.hits.replace(/\$\{hits}/, resultItems.length)
-
-            container.innerHTML = `
-              <div class="search-stats">${stats}</div>
-              <ul class="search-result-list">${resultItems.map(result => result.item).join('')}</ul>`
-          } else {
-            const stats = i18n.empty.replace(/\$\{query}/, searchText)
-            container.innerHTML = `<div class="search-stats">${stats}</div>`
-          }
-
-        }
-
-        const openPopup = () => {
-          utils.q('.trm-search-popup').classList.toggle('show')
-          setTimeout(() => input.focus(), 500)
-          if (!localSearch.isfetched)
-            localSearch.fetchData()
-        }
-
-        const colsePopup = function (e) {
-          utils.q('.trm-search-popup').classList.toggle('show')
-        }
-
-        const keypressEventFunction = function (e) {
-          if (e.key === 'Enter')
-            inputEventFunction()
-        }
-
-        if (search.trigger === 'auto') {
-          input.addEventListener('input', inputEventFunction)
-        }
-        else {
-          input.addEventListener('keypress', keypressEventFunction)
-        }
-
-        closeBtn.addEventListener('click', colsePopup)
-        searchBtn.addEventListener('click', openPopup)
-        document.addEventListener('swup:contentReplaced', (event) => {
-          searchBtn.removeEventListener('click', openPopup)
-          closeBtn.removeEventListener('click', colsePopup)
-          input.removeEventListener('input', inputEventFunction)
-          input.removeEventListener('keypress', keypressEventFunction)
-        });
-
-      }
-
     }
   }
 
@@ -413,9 +311,6 @@
 
   /* toc */
   utils.InitToc()
-
-  /* search  */
-  utils.InitSearch(true)
   //#endregion
 
   //#region  Re/init
@@ -453,8 +348,6 @@
     /* toc */
     utils.InitToc()
 
-    /* search  */
-    utils.InitSearch(true)
   });
   //#endregion
 
