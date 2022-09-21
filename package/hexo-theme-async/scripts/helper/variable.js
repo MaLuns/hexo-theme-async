d5062f8bb5e20aad4dc48e8b6c12342543dcec97
白云苍狗 <admin@imalun.com>
2022-09-21 14:42:51 +0800

    ✨ feat: Pjax下支持动态加载脚本插件

--- package/hexo-theme-async/scripts/helper/variable.js
+++ package/hexo-theme-async/scripts/helper/variable.js
@@ -1,6 +1,6 @@
 const { toI18n, isPlainObject } = require('../utils')
 
-function variableHelper(name, val, isLang) {
+function variableHelper(name, val, isLang = false, isSwupScript = false) {
     if (![null, undefined].includes(val) && typeof name === 'string') {
         if (isLang) {
             const { __ } = this;
@@ -10,7 +10,7 @@ function variableHelper(name, val, isLang) {
         let varStr = Array.isArray(val) || (isPlainObject(val) && val.toString === Object.prototype.toString)
             ? JSON.stringify(val, null, 2)
             : `'${String(val)}'`;
-        return `<script>window.${name}=${varStr}</script>`;
+        return `<script ${isSwupScript ? 'data-swup-reload-script' : ''}>window.${name}=${varStr}</script>`;
     }
 }
 
