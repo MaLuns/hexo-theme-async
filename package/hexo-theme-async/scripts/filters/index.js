2bcf60f2ee4b623e2a6f9dedaf6421230715974e
白云苍狗 <admin@imalun.com>
2022-09-22 11:28:08 +0800

    🐞 fix: 修复单闭合img懒加载替换错误

--- package/hexo-theme-async/scripts/filters/index.js
+++ package/hexo-theme-async/scripts/filters/index.js
@@ -1,7 +1,7 @@
 hexo.extend.filter.register('after_post_render', (data) => {
     // img lazyload
     if (hexo.theme.config.lazyload && hexo.theme.config.lazyload.enable) {
-        data.content = data.content.replace(/<img(.+?)>/gi, (str, p1) => {
+        data.content = data.content.replace(/<img(.+?)(\/{0,1})>/gi, (str, p1) => {
             return str.replace(p1, `${p1} loading="lazy"`)
         })
     }
