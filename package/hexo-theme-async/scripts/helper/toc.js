a97b939823269c61353aa47c21d4a667a780d68c
白云苍狗 <admin@imalun.com>
2022-10-10 18:16:30 +0800

    🎈 perf: 优化文章目录

--- package/hexo-theme-async/scripts/helper/toc.js
+++ package/hexo-theme-async/scripts/helper/toc.js
@@ -3,6 +3,8 @@
 const { tocObj, escapeHTML, encodeURL } = require('hexo-util');
 
 // 获取树状数据
+
+/* 
 const getTreeData = (list = []) => {
     let len = list.length;
     let st = [, , , , , ,];
@@ -45,7 +47,6 @@ const getTreeData = (list = []) => {
     });
     return res.filter(item => item.level === min);
 };
-
 
 function tocHelper(str, options = {}) {
     options = Object.assign({
@@ -107,6 +108,95 @@ function tocHelper(str, options = {}) {
     }
 
     return result;
+} */
+
+function tocHelper(str, options = {}) {
+  options = Object.assign({
+    min_depth: 1,
+    max_depth: 6,
+    class: 'toc',
+    class_item: '',
+    class_link: '',
+    class_text: '',
+    class_child: '',
+    class_number: '',
+    class_level: '',
+    list_number: true
+  }, options);
+
+  const data = tocObj(str, { min_depth: options.min_depth, max_depth: options.max_depth });
+
+  if (!data.length) return '';
+
+  const className = escapeHTML(options.class);
+  const itemClassName = escapeHTML(options.class_item || options.class + '-item');
+  const linkClassName = escapeHTML(options.class_link || options.class + '-link');
+  const textClassName = escapeHTML(options.class_text || options.class + '-text');
+  const childClassName = escapeHTML(options.class_child || options.class + '-child');
+  const numberClassName = escapeHTML(options.class_number || options.class + '-number');
+  const levelClassName = escapeHTML(options.class_level || options.class + '-level');
+  const listNumber = options.list_number;
+
+  let result = `<ol class="${className}">`;
+
+  const lastNumber = [0, 0, 0, 0, 0, 0];
+  let firstLevel = 0;
+  let lastLevel = 0;
+
+  for (let i = 0, len = data.length; i < len; i++) {
+    const el = data[i];
+    const { level, id, text } = el;
+    const href = id ? `#${id}` : null;
+
+    if (!el.unnumbered) {
+      lastNumber[level - 1]++;
+    }
+
+    for (let i = level; i <= 5; i++) {
+      lastNumber[i] = 0;
+    }
+
+    if (firstLevel) {
+      for (let i = level; i < lastLevel; i++) {
+        result += '</li></ol>';
+      }
+
+      if (level > lastLevel) {
+        result += `<ol class="${childClassName}">`;
+      } else {
+        result += '</li>';
+      }
+    } else {
+      firstLevel = level;
+    }
+
+    result += `<li class="${itemClassName} ${levelClassName}-${level}">`;
+    if (href) {
+      result += `<a rel="nofollow" class="${linkClassName}" href="${href}"  data-scroll-to="${href}">`;
+    } else {
+      result += `<a class="${linkClassName}">`;
+    }
+
+    if (listNumber && !el.unnumbered) {
+      result += `<span class="${numberClassName}">`;
+
+      for (let i = firstLevel - 1; i < level; i++) {
+        result += `${lastNumber[i]}.`;
+      }
+
+      result += '</span> ';
+    }
+
+    result += `<span class="${textClassName}">${text}</span></a>`;
+
+    lastLevel = level;
+  }
+
+  for (let i = firstLevel - 1; i < lastLevel; i++) {
+    result += '</li></ol>';
+  }
+
+  return result;
 }
 
 
