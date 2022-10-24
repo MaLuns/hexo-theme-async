22d4f89a68551bb339a41fb59d505f6ced3523fe
白云苍狗 <admin@imalun.com>
2022-10-24 13:29:28 +0800

    🐞 fix: 除去发布 npm 版本多了个v

--- script/publish.js
+++ script/publish.js
@@ -5,7 +5,7 @@ if (process.env.RELEASE_VERSION) {
     const version = process.env.RELEASE_VERSION.split('/').reverse()[0]
     console.log('当前版本：', version);
     const pkg = require('../package/hexo-theme-async/package.json')
-    pkg.version = version
+    pkg.version = version.replace('v', '')
     fs.writeFileSync(path.resolve(__dirname, '../package/hexo-theme-async/package.json'), JSON.stringify(pkg, null, 4), 'utf-8')
 }
 
