29029a468e44da45bc0c66c9a7a74de7ff7809cf
白云苍狗 <admin@imalun.com>
2022-12-31 16:59:31 +0800

    ✨ feat: 增加 caniuse 标签插件

---
+++ package/hexo-theme-async/scripts/tags/caniuse.js
@@ -0,0 +1,19 @@
+/**
+ * Can i use
+ * transplant from hexo-theme-next
+ */
+
+'use strict';
+
+function caniUse(args) {
+    const [feature, periods = 'current'] = args.join('').split('@');
+
+    if (!feature) {
+        hexo.log.warn('Caniuse feature can NOT be empty.');
+        return '';
+    }
+
+    return `<iframe data-feature="${feature}" src="https://caniuse.bitsofco.de/embed/index.html?feat=${feature}&periods=${periods}&accessible-colours=false" frameborder="0" width="100%" height="400px"></iframe>`;
+};
+
+hexo.extend.tag.register('caniuse', caniUse);
