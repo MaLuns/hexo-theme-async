8aaea63b90f051837dbfb55c42b82727cbd39839
MaLuns <MaLuns@users.noreply.github.com>
2023-01-09 06:18:38 +0000

    deploy: 42a1ad7cd01b0e7f27937f6aa713dd224133e6e2

---
+++ demosite/demo.js
@@ -0,0 +1,5 @@
+const log = (str) => console.log(str,
+    'color: white; background: #0078E7; padding:5px 0;margin: 0 0 2px 0;border-radius: 4px 0 0 4px;',
+    'padding: 4px;border:1px solid #0078E7;border-radius: 0 4px 4px 0; background: linear-gradient(70deg, #e3f9eb, #d1dbff);',
+)
+log(`%c 📌 添加自定义脚本 %c 这里啥也没有，测试自定义脚本执行`)
