0716cd6343e646a4d25415ba52a41a4605b8dda4
白云苍狗 <admin@imalun.com>
2023-03-31 11:38:12 +0800

    🐳 chore: 增加 demosite 校验

--- docs/.vitepress/components/DemoSites.vue
+++ docs/.vitepress/components/DemoSites.vue
@@ -1,33 +1,34 @@
-<script  setup>
-import { ref } from 'vue'
-import sites from '../assets/sites.json'
+<script setup>
+import { ref } from "vue";
+import sites from "../assets/sites.json";
 
-const numOfRow = ref(6)
+const siteUseThemes = sites.filter((item) => item.type === undefined);
+const numOfRow = ref(6);
 </script>
 
 <template>
-    <table align="center">
-        <tr v-for="row in Math.ceil(sites.length / numOfRow)" :key="row" align="center">
-            <template v-for="col in numOfRow">
-                <td v-if="sites[(row - 1) * numOfRow + col - 1]" :key="col" class="px-1 py-2">
-                    <demo-site :site="sites[(row - 1) * numOfRow + col - 1]" />
-                </td>
-            </template>
-        </tr>
-    </table>
+	<table align="center">
+		<tr v-for="row in Math.ceil(siteUseThemes.length / numOfRow)" :key="row" align="center">
+			<template v-for="col in numOfRow">
+				<td v-if="siteUseThemes[(row - 1) * numOfRow + col - 1]" :key="col" class="px-1 py-2">
+					<demo-site :site="sites[(row - 1) * numOfRow + col - 1]" />
+				</td>
+			</template>
+		</tr>
+	</table>
 </template>
 
 <style lang="less" scoped>
 .site-list {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, 120px);
-    grid-gap: 10px;
-    list-style: none !important;
-    margin: 0px !important;
-    padding: 0px !important;
+	display: grid;
+	grid-template-columns: repeat(auto-fill, 120px);
+	grid-gap: 10px;
+	list-style: none !important;
+	margin: 0px !important;
+	padding: 0px !important;
 
-    li {
-        border: 1px solid #000;
-    }
+	li {
+		border: 1px solid #000;
+	}
 }
 </style>
---
+++ script/sites-check.js
@@ -0,0 +1,81 @@
+const demosite = require('../docs/.vitepress/assets/sites.json')
+const https = require("https")
+const fs = require('fs')
+const path = require('path')
+
+const generateDemoSiteHtml = (data) => {
+    let str = `<!-- demo-start -->
+<table>
+  <tr align="center">`
+    data.forEach((item, index) => {
+        if (index % 8 === 0 && index > 0) {
+            str += `
+    </tr>
+    <tr align="center">`
+        }
+        str += `
+        <td>
+            <a href="${item.url}" target="_blank">
+                <img width="80px" src="${item.avatar}" />
+                <br />
+                <sub title="${item.desc}">${item.name}</sub>
+            </a>
+        </td>`
+    })
+    str += `
+      </tr>
+</table>
+<!-- demo-end -->`
+    return str;
+}
+
+const updateReadme = (sites) => {
+    const paths = [path.resolve(__dirname, '../README.md'), path.resolve(__dirname, '../README_zh-CN.md')]
+    paths.forEach(item => {
+        let readme = fs.readFileSync(item, 'utf-8')
+        readme = readme.replace(/<!-- demo-start -->(.)+?<!-- demo-end -->/gms, () => generateDemoSiteHtml(sites))
+        fs.writeFileSync(item, readme, 'utf-8')
+    })
+}
+
+const check = (data) => {
+    return new Promise((resolve, reject) => {
+        const req = https.get(data.url, function (res) {
+            let html = ''
+            res.setEncoding("utf-8")
+            if (res.statusCode >= 200 && res.statusCode < 300) {
+                res.on("data", function (data) {
+                    html += data
+                })
+                res.on("end", () => {
+                    if (!/ASYNC_CONFIG/.test(html)) {
+                        console.log('校验失败: ', data.url);
+                        data.type = -2
+                    } else {
+                        console.log('校验通过: ', data.url);
+                        delete data.type
+                    }
+                })
+            } else {
+                console.log('站点无法访问: ', data.url);
+                data.type = -1
+            }
+        })
+        req.on("close", () => {
+            resolve()
+        })
+        req.on("error", () => {
+            console.log('站点无法访问: ', data.url);
+            data.type = -1
+            resolve()
+        })
+    })
+}
+
+const checkDemoSites = () => Promise.allSettled(demosite.map(item => check(item))).then(res => {
+    updateReadme(demosite.filter(item => item.type === undefined))
+    const filepath = path.resolve(__dirname, '../docs/.vitepress/assets/sites.json')
+    fs.writeFileSync(filepath, JSON.stringify(demosite, null, 4))
+})
+
+checkDemoSites()
