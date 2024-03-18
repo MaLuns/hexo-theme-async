4d870a80f3872da69d380d45da8e18b899eee492
MaLuns <MaLuns@users.noreply.github.com>
2024-03-18 07:39:59 +0000

    deploy: 54935ba235f17a66007f1851f4ace293d0d56953

---
+++ assets/guide_config.md.e7f4b959.lean.js
@@ -0,0 +1,116 @@
+import{_ as g}from"./chunks/IconList.c56dd75f.js";import{c as E}from"./chunks/config.f6d32a6d.js";import{o as u,c as b,k as s,t as n,l as t,H as F,Q as a,a as o}from"./chunks/framework.d3e5bc77.js";const h=`swup: /swup@2.0.19/dist/swup.min.js
+fancybox: 
+  css: /@fancyapps/ui@4.0/dist/fancybox.css
+  js: /@fancyapps/ui@4.0/dist/fancybox.umd.js
+search: /hexo-generator-searchdb@1.4.0/dist/search.js
+swiper: 
+  css: /swiper@4.5.1/dist/css/swiper.min.css
+  js: /swiper@4.5.1/dist/js/swiper.min.js
+katex:
+  js: /katex@latest/dist/katex.min.js
+  css: /katex@latest/dist/katex.min.css
+  auto_render: /katex@latest/dist/contrib/auto-render.min.js
+  copy_tex: /katex@latest/dist/contrib/copy-tex.min.js
+bComments: [
+  //imgcache.qq.com/qcloud/cloudbase-js-sdk/1.7.1/cloudbase.full.js ,
+  /b-comments@0.0.13/dist/index.all.min.js
+]
+twikoo: /twikoo@1.6.7/dist/twikoo.all.min.js
+flickr_justified_gallery: /flickr-justified-gallery@latest/dist/fjGallery.min.js`,_=`site:
+  title: 本站信息
+  webmaster: 网站名称
+  domain: 网站链接
+  avatar: 网站头像
+  describe: 网站简介
+  ruleText: 友链要求
+
+sidebar:
+  residence: Residence
+  city: City
+  age: Age
+  contact: 联系我
+
+footer:
+  powered: '由 %s 驱动'
+  theme: 主题
+  tips: '博客已萌萌哒运行 %s 天'
+  day: 天
+  hour: 时
+  minute: 分
+  seconds: 秒
+
+title:
+  links: 友情链接
+  new_publish: 最近发布
+  comment: 留言板
+  author: 作者
+  blog: 博客
+  privacy: 隐私权及评论
+  more: 查看分类
+  all_archives: 所有归档
+  year_archives: '%s 年的归档'
+  other_articles: 其他文章
+  unclassified: 未分类
+
+menu:
+  home: 首页
+  archives: 归档
+  links: 友链
+  about: 关于
+
+favicon:
+  showText: (/≧▽≦/)咦！又好了！
+  hideText: (●—●)喔哟，崩溃啦！
+
+search:
+  placeholder: 搜索文章...
+  result_placeholder: 请输入关键词进行搜索
+  empty: '找不到您查询的内容: \${query}'
+  hits: '找到 \${hits} 条结果'
+  hits_time: '找到 \${hits} 条结果（用时 \${time} 毫秒）'
+
+reward:
+  comment: 我很可爱，请给我钱
+
+post:
+  sticky: 置顶
+  more: 更多
+  copyright:
+    author: 本文作者
+    link: 本文链接
+    license_title: 版权声明
+    license_content: '本博客所有文章除特别声明外，均默认采用 %s 许可协议。'
+  copy:
+    success: 复制成功
+    failure: 复制失败
+  read_mode:
+    open: 进入阅读模式
+    exit: 退出阅读模式
+  notice_outdate_message: '距离上次更新已经 %s 天了, 文章内容可能已经过时。'
+  toc: 目录导航
+
+rightside:
+  toc_title: 目录
+  readmode_title: 阅读模式
+  back_to_top: 回到顶部
+  aside: 单栏和双栏切换
+
+date_suffix:
+  just: 刚刚
+  min: 分钟前
+  hour: 小时前
+  day: 天前
+  month: 个月前
+
+symbol:
+  comma: ，
+  period: 。
+  colon: ：
+
+ieTips: 为了更好的体验,请使用非IE浏览器
+
+not_found: 
+	name: 主页
+	text: 您正在查找的页面不存在。您是怎么到这里来的是个谜,但是您可以点击下面的按钮返回主页。`,m=a("",13),v={class:"details custom-block"},C=s("summary",null,"内置语言包结构示例",-1),A={class:"language-yaml vp-adaptive-theme"},k=s("button",{title:"Copy Code",class:"copy"},null,-1),f=s("span",{class:"lang"},"yaml",-1),D={class:"shiki github-dark vp-code-dark"},T={class:"line"},B={style:{color:"#9ECBFF"}},q={class:"shiki github-light vp-code-light"},S={class:"line"},x={style:{color:"#032F62"}},P=a("",4),I={class:"vp-code-group vp-adaptive-theme"},w=a("",1),V={class:"blocks"},N=a("",1),R={class:"language-yaml vp-adaptive-theme"},j=s("button",{title:"Copy Code",class:"copy"},null,-1),H=s("span",{class:"lang"},"yaml",-1),z={class:"shiki github-dark vp-code-dark"},W={class:"line"},M={style:{color:"#9ECBFF"}},J={class:"shiki github-light vp-code-light"},O={class:"line"},U={style:{color:"#032F62"}},Y=a("",3),G={class:"vp-code-group vp-adaptive-theme"},X=a("",1),Z={class:"blocks"},L=a("",1),K={class:"language-yaml vp-adaptive-theme"},Q=s("button",{title:"Copy Code",class:"copy"},null,-1),$=s("span",{class:"lang"},"yaml",-1),ss={class:"shiki github-dark vp-code-dark"},as={class:"line"},ns={style:{color:"#9ECBFF"}},ls={class:"shiki github-light vp-code-light"},ps={class:"line"},os={style:{color:"#032F62"}},es=a("",3),ts={class:"vp-code-group vp-adaptive-theme"},cs=a("",1),rs={class:"blocks"},is=a("",1),ys={class:"language-yaml vp-adaptive-theme"},Es=s("button",{title:"Copy Code",class:"copy"},null,-1),ds=s("span",{class:"lang"},"yaml",-1),hs={class:"shiki github-dark vp-code-dark"},_s={class:"line"},gs={style:{color:"#9ECBFF"}},us={class:"shiki github-light vp-code-light"},bs={class:"line"},Fs={style:{color:"#032F62"}},ms=a("",6),vs={class:"details custom-block"},Cs=s("summary",null,"主题使用三方插件列表",-1),As={class:"language-yaml vp-adaptive-theme"},ks=s("button",{title:"Copy Code",class:"copy"},null,-1),fs=s("span",{class:"lang"},"yaml",-1),Ds={class:"shiki github-dark vp-code-dark"},Ts={class:"line"},Bs={style:{color:"#9ECBFF"}},qs={class:"shiki github-light vp-code-light"},Ss={class:"line"},xs={style:{color:"#032F62"}},Ps=a("",5),Is={class:"vp-code-group vp-adaptive-theme"},ws=a("",1),Vs={class:"blocks"},Ns=a("",1),Rs={class:"language-yaml vp-adaptive-theme"},js=s("button",{title:"Copy Code",class:"copy"},null,-1),Hs=s("span",{class:"lang"},"yaml",-1),zs={class:"shiki github-dark vp-code-dark"},Ws={class:"line"},Ms={style:{color:"#9ECBFF"}},Js={class:"shiki github-light vp-code-light"},Os={class:"line"},Us={style:{color:"#032F62"}},Ys=a("",15),Gs={class:"vp-code-group vp-adaptive-theme"},Xs=a("",1),Zs={class:"blocks"},Ls=a("",1),Ks={class:"language-yaml vp-adaptive-theme"},Qs=s("button",{title:"Copy Code",class:"copy"},null,-1),$s=s("span",{class:"lang"},"yaml",-1),sa={class:"shiki github-dark vp-code-dark"},aa={class:"line"},na={style:{color:"#9ECBFF"}},la={class:"shiki github-light vp-code-light"},pa={class:"line"},oa={style:{color:"#032F62"}},ea=a("",3),ta={class:"vp-code-group vp-adaptive-theme"},ca=a("",1),ra={class:"blocks"},ia=a("",1),ya={class:"language-yaml vp-adaptive-theme"},Ea=s("button",{title:"Copy Code",class:"copy"},null,-1),da=s("span",{class:"lang"},"yaml",-1),ha={class:"shiki github-dark vp-code-dark"},_a={class:"line"},ga={style:{color:"#9ECBFF"}},ua={class:"shiki github-light vp-code-light"},ba={class:"line"},Fa={style:{color:"#032F62"}},ma=a("",4),va={class:"vp-code-group vp-adaptive-theme"},Ca=a("",1),Aa={class:"blocks"},ka=a("",1),fa={class:"language-yaml vp-adaptive-theme"},Da=s("button",{title:"Copy Code",class:"copy"},null,-1),Ta=s("span",{class:"lang"},"yaml",-1),Ba={class:"shiki github-dark vp-code-dark"},qa={class:"line"},Sa={style:{color:"#9ECBFF"}},xa={class:"shiki github-light vp-code-light"},Pa={class:"line"},Ia={style:{color:"#032F62"}},wa=a("",11),Va={class:"vp-code-group vp-adaptive-theme"},Na=a("",1),Ra={class:"blocks"},ja=a("",1),Ha={class:"language-yaml vp-adaptive-theme"},za=s("button",{title:"Copy Code",class:"copy"},null,-1),Wa=s("span",{class:"lang"},"yaml",-1),Ma={class:"shiki github-dark vp-code-dark"},Ja={class:"line"},Oa={style:{color:"#9ECBFF"}},Ua={class:"shiki github-light vp-code-light"},Ya={class:"line"},Ga={style:{color:"#032F62"}},Xa=a("",32),Za={class:"vp-code-group vp-adaptive-theme"},La=a("",1),Ka={class:"blocks"},Qa=a("",1),$a={class:"language-yaml vp-adaptive-theme"},sn=s("button",{title:"Copy Code",class:"copy"},null,-1),an=s("span",{class:"lang"},"yaml",-1),nn={class:"shiki github-dark vp-code-dark"},ln={class:"line"},pn={style:{color:"#9ECBFF"}},on={class:"shiki github-light vp-code-light"},en={class:"line"},tn={style:{color:"#032F62"}},cn=a("",5),rn={class:"vp-code-group vp-adaptive-theme"},yn=a("",1),En={class:"blocks"},dn=a("",1),hn={class:"language-yaml vp-adaptive-theme"},_n=s("button",{title:"Copy Code",class:"copy"},null,-1),gn=s("span",{class:"lang"},"yaml",-1),un={class:"shiki github-dark vp-code-dark"},bn={class:"line"},Fn={style:{color:"#9ECBFF"}},mn={class:"shiki github-light vp-code-light"},vn={class:"line"},Cn={style:{color:"#032F62"}},An=a("",6),kn={class:"vp-code-group vp-adaptive-theme"},fn=a("",1),Dn={class:"blocks"},Tn=a("",1),Bn={class:"language-yaml vp-adaptive-theme"},qn=s("button",{title:"Copy Code",class:"copy"},null,-1),Sn=s("span",{class:"lang"},"yaml",-1),xn={class:"shiki github-dark vp-code-dark"},Pn={class:"line"},In={style:{color:"#9ECBFF"}},wn={class:"shiki github-light vp-code-light"},Vn={class:"line"},Nn={style:{color:"#032F62"}},Rn=a("",4),jn={class:"vp-code-group vp-adaptive-theme"},Hn=a("",1),zn={class:"blocks"},Wn=a("",1),Mn={class:"language-yaml vp-adaptive-theme"},Jn=s("button",{title:"Copy Code",class:"copy"},null,-1),On=s("span",{class:"lang"},"yaml",-1),Un={class:"shiki github-dark vp-code-dark"},Yn={class:"line"},Gn={style:{color:"#9ECBFF"}},Xn={class:"shiki github-light vp-code-light"},Zn={class:"line"},Ln={style:{color:"#032F62"}},Kn=a("",5),Qn={class:"vp-code-group vp-adaptive-theme"},$n=a("",1),sl={class:"blocks"},al=a("",1),nl={class:"language-yaml vp-adaptive-theme"},ll=s("button",{title:"Copy Code",class:"copy"},null,-1),pl=s("span",{class:"lang"},"yaml",-1),ol={class:"shiki github-dark vp-code-dark"},el={class:"line"},tl={style:{color:"#9ECBFF"}},cl={class:"shiki github-light vp-code-light"},rl={class:"line"},il={style:{color:"#032F62"}},yl=s("h3",{id:"上下篇文章",tabindex:"-1"},[o("上下篇文章 "),s("a",{class:"header-anchor",href:"#上下篇文章","aria-label":'Permalink to "上下篇文章"'},"​")],-1),El=s("ul",null,[s("li",null,[s("code",null,"enable"),o(": 是否开启")]),s("li",null,[s("code",null,"type"),o(": 卡片类型，可选 "),s("code",null,"large"),o(" || "),s("code",null,"small")])],-1),dl={class:"vp-code-group vp-adaptive-theme"},hl=a("",1),_l={class:"blocks"},gl=a("",1),ul={class:"language-yaml vp-adaptive-theme"},bl=s("button",{title:"Copy Code",class:"copy"},null,-1),Fl=s("span",{class:"lang"},"yaml",-1),ml={class:"shiki github-dark vp-code-dark"},vl={class:"line"},Cl={style:{color:"#9ECBFF"}},Al={class:"shiki github-light vp-code-light"},kl={class:"line"},fl={style:{color:"#032F62"}},Dl=a("",3),Tl={class:"vp-code-group vp-adaptive-theme"},Bl=a("",1),ql={class:"blocks"},Sl=a("",1),xl={class:"language-yaml vp-adaptive-theme"},Pl=s("button",{title:"Copy Code",class:"copy"},null,-1),Il=s("span",{class:"lang"},"yaml",-1),wl={class:"shiki github-dark vp-code-dark"},Vl={class:"line"},Nl={style:{color:"#9ECBFF"}},Rl={class:"shiki github-light vp-code-light"},jl={class:"line"},Hl={style:{color:"#032F62"}},zl=s("h3",{id:"文章破图时默认图片",tabindex:"-1"},[o("文章破图时默认图片 "),s("a",{class:"header-anchor",href:"#文章破图时默认图片","aria-label":'Permalink to "文章破图时默认图片"'},"​")],-1),Wl=s("p",null,"您可以添加图片加载失败时显示默认图片。",-1),Ml=s("ul",null,[s("li",null,[s("code",null,"flink"),o(": 友链头像破图时显示默认图片")]),s("li",null,[s("code",null,"post_page"),o(": 文章中图片破图时显示默认图片")])],-1),Jl={class:"vp-code-group vp-adaptive-theme"},Ol=a("",1),Ul={class:"blocks"},Yl=a("",1),Gl={class:"language-yaml vp-adaptive-theme"},Xl=s("button",{title:"Copy Code",class:"copy"},null,-1),Zl=s("span",{class:"lang"},"yaml",-1),Ll={class:"shiki github-dark vp-code-dark"},Kl={class:"line"},Ql={style:{color:"#9ECBFF"}},$l={class:"shiki github-light vp-code-light"},sp={class:"line"},ap={style:{color:"#032F62"}},np=a("",6),lp={class:"vp-code-group vp-adaptive-theme"},pp=a("",1),op={class:"blocks"},ep=a("",1),tp={class:"language-yaml vp-adaptive-theme"},cp=s("button",{title:"Copy Code",class:"copy"},null,-1),rp=s("span",{class:"lang"},"yaml",-1),ip={class:"shiki github-dark vp-code-dark"},yp={class:"line"},Ep={style:{color:"#9ECBFF"}},dp={class:"shiki github-light vp-code-light"},hp={class:"line"},_p={style:{color:"#032F62"}},gp=s("h3",{id:"固定按钮块",tabindex:"-1"},[o("固定按钮块 "),s("a",{class:"header-anchor",href:"#固定按钮块","aria-label":'Permalink to "固定按钮块"'},"​")],-1),up=s("p",null,"右下角悬浮按钮块。",-1),bp=s("ul",null,[s("li",null,[s("code",null,"readmode"),o(": 阅读模式按钮")]),s("li",null,[s("code",null,"aside"),o(": 单双栏切换按钮")])],-1),Fp={class:"vp-code-group vp-adaptive-theme"},mp=a("",1),vp={class:"blocks"},Cp=a("",1),Ap={class:"language-yaml vp-adaptive-theme"},kp=s("button",{title:"Copy Code",class:"copy"},null,-1),fp=s("span",{class:"lang"},"yaml",-1),Dp={class:"shiki github-dark vp-code-dark"},Tp={class:"line"},Bp={style:{color:"#9ECBFF"}},qp={class:"shiki github-light vp-code-light"},Sp={class:"line"},xp={style:{color:"#032F62"}},Pp=a("",3),Ip={class:"vp-code-group vp-adaptive-theme"},wp=a("",1),Vp={class:"blocks"},Np=a("",1),Rp={class:"language-yaml vp-adaptive-theme"},jp=s("button",{title:"Copy Code",class:"copy"},null,-1),Hp=s("span",{class:"lang"},"yaml",-1),zp={class:"shiki github-dark vp-code-dark"},Wp={class:"line"},Mp={style:{color:"#9ECBFF"}},Jp={class:"shiki github-light vp-code-light"},Op={class:"line"},Up={style:{color:"#032F62"}},Yp=a("",3),Gp={class:"vp-code-group vp-adaptive-theme"},Xp=a("",1),Zp={class:"blocks"},Lp=a("",1),Kp={class:"language-yaml vp-adaptive-theme"},Qp=s("button",{title:"Copy Code",class:"copy"},null,-1),$p=s("span",{class:"lang"},"yaml",-1),so={class:"shiki github-dark vp-code-dark"},ao={class:"line"},no={style:{color:"#9ECBFF"}},lo={class:"shiki github-light vp-code-light"},po={class:"line"},oo={style:{color:"#032F62"}},eo=a("",9),to=a("",39),co={class:"details custom-block"},ro=s("summary",null,"完整配置列表信息.",-1),io={class:"language-yaml vp-adaptive-theme"},yo=s("button",{title:"Copy Code",class:"copy"},null,-1),Eo=s("span",{class:"lang"},"yaml",-1),ho={class:"shiki github-dark vp-code-dark"},_o={class:"line"},go={style:{color:"#9ECBFF"}},uo={class:"shiki github-light vp-code-light"},bo={class:"line"},Fo={style:{color:"#032F62"}},fo=JSON.parse('{"title":"主题配置","description":"","frontmatter":{},"headers":[],"relativePath":"guide/config.md","filePath":"guide/config.md","lastUpdated":1710747449000}'),mo={name:"guide/config.md"},Do=Object.assign(mo,{setup(vo){const r=E.split(`
+`),d=[];let y=null;for(let e=0;e<r.length;e++){let p=r[e];if(y!==null&&!p.startsWith(" "))break;y!==null&&!p.trim().startsWith("#")&&d.push(p.split(":")[1].trim()),p==="icons:"&&(y=e+1)}const l=e=>{const p=[];for(let c=0;c<r.length;c++){let i=r[c];if(p.length){if(!i.startsWith(" "))break;p.push(i)}i.trimEnd()===`${e}:`&&p.push(i)}return p.join(`
+`)};return(e,p)=>{const c=g;return u(),b("div",null,[m,s("details",v,[C,s("div",A,[k,f,s("pre",D,[s("code",null,[s("span",T,[s("span",B,n(t(_)),1)])])]),s("pre",q,[s("code",null,[s("span",S,[s("span",x,n(t(_)),1)])])])])]),P,s("div",I,[w,s("div",V,[N,s("div",R,[j,H,s("pre",z,[s("code",null,[s("span",W,[s("span",M,n(l("theme")),1)])])]),s("pre",J,[s("code",null,[s("span",O,[s("span",U,n(l("theme")),1)])])])])])]),Y,s("div",G,[X,s("div",Z,[L,s("div",K,[Q,$,s("pre",ss,[s("code",null,[s("span",as,[s("span",ns,n(l("favicon")),1)])])]),s("pre",ls,[s("code",null,[s("span",ps,[s("span",os,n(l("favicon")),1)])])])])])]),es,s("div",ts,[cs,s("div",rs,[is,s("div",ys,[Es,ds,s("pre",hs,[s("code",null,[s("span",_s,[s("span",gs,n(l("assets")),1)])])]),s("pre",us,[s("code",null,[s("span",bs,[s("span",Fs,n(l("assets")),1)])])])])])]),ms,s("details",vs,[Cs,s("div",As,[ks,fs,s("pre",Ds,[s("code",null,[s("span",Ts,[s("span",Bs,n(t(h)),1)])])]),s("pre",qs,[s("code",null,[s("span",Ss,[s("span",xs,n(t(h)),1)])])])])]),Ps,s("div",Is,[ws,s("div",Vs,[Ns,s("div",Rs,[js,Hs,s("pre",zs,[s("code",null,[s("span",Ws,[s("span",Ms,n(l("cdn")),1)])])]),s("pre",Js,[s("code",null,[s("span",Os,[s("span",Us,n(l("cdn")),1)])])])])])]),Ys,s("div",Gs,[Xs,s("div",Zs,[Ls,s("div",Ks,[Qs,$s,s("pre",sa,[s("code",null,[s("span",aa,[s("span",na,n(l("user")),1)])])]),s("pre",la,[s("code",null,[s("span",pa,[s("span",oa,n(l("user")),1)])])])])])]),ea,s("div",ta,[ca,s("div",ra,[ia,s("div",ya,[Ea,da,s("pre",ha,[s("code",null,[s("span",_a,[s("span",ga,n(l("top_bars")),1)])])]),s("pre",ua,[s("code",null,[s("span",ba,[s("span",Fa,n(l("top_bars")),1)])])])])])]),ma,s("div",va,[Ca,s("div",Aa,[ka,s("div",fa,[Da,Ta,s("pre",Ba,[s("code",null,[s("span",qa,[s("span",Sa,n(l("sidebar")),1)])])]),s("pre",xa,[s("code",null,[s("span",Pa,[s("span",Ia,n(l("sidebar")),1)])])])])])]),wa,s("div",Va,[Na,s("div",Ra,[ja,s("div",Ha,[za,Wa,s("pre",Ma,[s("code",null,[s("span",Ja,[s("span",Oa,n(l("banner")),1)])])]),s("pre",Ua,[s("code",null,[s("span",Ya,[s("span",Ga,n(l("banner")),1)])])])])])]),Xa,s("div",Za,[La,s("div",Ka,[Qa,s("div",$a,[sn,an,s("pre",nn,[s("code",null,[s("span",ln,[s("span",pn,n(l("reward")),1)])])]),s("pre",on,[s("code",null,[s("span",en,[s("span",tn,n(l("reward")),1)])])])])])]),cn,s("div",rn,[yn,s("div",En,[dn,s("div",hn,[_n,gn,s("pre",un,[s("code",null,[s("span",bn,[s("span",Fn,n(l("toc")),1)])])]),s("pre",mn,[s("code",null,[s("span",vn,[s("span",Cn,n(l("toc")),1)])])])])])]),An,s("div",kn,[fn,s("div",Dn,[Tn,s("div",Bn,[qn,Sn,s("pre",xn,[s("code",null,[s("span",Pn,[s("span",In,n(l("archive")),1)])])]),s("pre",wn,[s("code",null,[s("span",Vn,[s("span",Nn,n(l("archive")),1)])])])])])]),Rn,s("div",jn,[Hn,s("div",zn,[Wn,s("div",Mn,[Jn,On,s("pre",Un,[s("code",null,[s("span",Yn,[s("span",Gn,n(l("creative_commons")),1)])])]),s("pre",Xn,[s("code",null,[s("span",Zn,[s("span",Ln,n(l("creative_commons")),1)])])])])])]),Kn,s("div",Qn,[$n,s("div",sl,[al,s("div",nl,[ll,pl,s("pre",ol,[s("code",null,[s("span",el,[s("span",tl,n(l("cover")),1)])])]),s("pre",cl,[s("code",null,[s("span",rl,[s("span",il,n(l("cover")),1)])])])])])]),yl,El,s("div",dl,[hl,s("div",_l,[gl,s("div",ul,[bl,Fl,s("pre",ml,[s("code",null,[s("span",vl,[s("span",Cl,n(l("post_pagination")),1)])])]),s("pre",Al,[s("code",null,[s("span",kl,[s("span",fl,n(l("post_pagination")),1)])])])])])]),Dl,s("div",Tl,[Bl,s("div",ql,[Sl,s("div",xl,[Pl,Il,s("pre",wl,[s("code",null,[s("span",Vl,[s("span",Nl,n(l("notice_outdate")),1)])])]),s("pre",Rl,[s("code",null,[s("span",jl,[s("span",Hl,n(l("notice_outdate")),1)])])])])])]),zl,Wl,Ml,s("div",Jl,[Ol,s("div",Ul,[Yl,s("div",Gl,[Xl,Zl,s("pre",Ll,[s("code",null,[s("span",Kl,[s("span",Ql,n(l("error_img")),1)])])]),s("pre",$l,[s("code",null,[s("span",sp,[s("span",ap,n(l("error_img")),1)])])])])])]),np,s("div",lp,[pp,s("div",op,[ep,s("div",tp,[cp,rp,s("pre",ip,[s("code",null,[s("span",yp,[s("span",Ep,n(l("categorie_card")),1)])])]),s("pre",dp,[s("code",null,[s("span",hp,[s("span",_p,n(l("categorie_card")),1)])])])])])]),gp,up,bp,s("div",Fp,[mp,s("div",vp,[Cp,s("div",Ap,[kp,fp,s("pre",Dp,[s("code",null,[s("span",Tp,[s("span",Bp,n(l("rightside")),1)])])]),s("pre",qp,[s("code",null,[s("span",Sp,[s("span",xp,n(l("rightside")),1)])])])])])]),Pp,s("div",Ip,[wp,s("div",Vp,[Np,s("div",Rp,[jp,Hp,s("pre",zp,[s("code",null,[s("span",Wp,[s("span",Mp,n(l("datetime_foramt")),1)])])]),s("pre",Jp,[s("code",null,[s("span",Op,[s("span",Up,n(l("datetime_foramt")),1)])])])])])]),Yp,s("div",Gp,[Xp,s("div",Zp,[Lp,s("div",Kp,[Qp,$p,s("pre",so,[s("code",null,[s("span",ao,[s("span",no,n(l("highlight")),1)])])]),s("pre",lo,[s("code",null,[s("span",po,[s("span",oo,n(l("highlight")),1)])])])])])]),eo,F(c,{title:"主题使用图标预览",prefix:"",icons:d}),to,s("details",co,[ro,s("div",io,[yo,Eo,s("pre",ho,[s("code",null,[s("span",_o,[s("span",go,n(t(E)),1)])])]),s("pre",uo,[s("code",null,[s("span",bo,[s("span",Fo,n(t(E)),1)])])])])])])}}});export{fo as __pageData,Do as default};
